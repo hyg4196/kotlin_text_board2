581b1e162abb815c3c9238aee3d631596dd0d06e
hyg1488 <hyg1488@gmail.com>
2021-04-14 20:29:27 +0900

    명령어 입력 기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
+    println("안녕하세요.")
+    while(true){
+        print("명령어")
+        val command = readLine()
+        println("입력한 명령어 : ${command}")
+    }
 
+    println("수고하세요")
 }
